87c176e4efed5015465cc13818e3543615ed71cb
nawal hajoui <hajouinawal@gmail.com>
2022-01-27 16:53:09 +0100

    creation partie frontend

--- frontend/bioWarranty/src/app/app.module.ts
+++ frontend/bioWarranty/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ProduitsComponent } from './components/produits/produits.component';
+import { AuthentificationComponent } from './components/authentification/authentification.component';
+import { PanierComponent } from './components/panier/panier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProduitsComponent,
+    AuthentificationComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
